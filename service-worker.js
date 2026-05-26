17a652ee4f67950b089d3cb081842ac597720091
Cesar Santana <137285737+cesarsant85@users.noreply.github.com>
2026-05-26 13:39:31 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'calculadora-mercado-v1';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './imagens/icon-192.png',
+  './imagens/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
